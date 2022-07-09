5a4bdbc9d5f796176d0ec3fccab4c50fabcf6e4d
Dalian Grullon <dalian.business@aol.com>
2022-07-09 16:58:32 -0400

    added BMR calculation and user object update for recieving BMR. Also adjust height and weight input fields to accept decimal values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let userData = {
     height: null, // will be populated on submit of userform
     age: null, // will be populated on submit of userform
     goal: ['', null], // will be populated on submit of userform
+    BMR: null
 }
 
 function openNav() {
--- assets/js/userForm.js
+++ assets/js/userForm.js
@@ -7,6 +7,8 @@ let genderInput = document.getElementById('gender');
 let goalButtons = document.getElementsByName('fit_goal'); // is an array
 let goalValEl = document.getElementById('goal-val');
 let goalVal = document.createElement('input');
+const kilogramPerPound = 0.453592;
+const centimeterPerFoot = 30.48;
 
 
 function addGoalValue() {
@@ -35,7 +37,21 @@ function updateUserObject(e){
     userData.age = ageInput.value;
     userData.goal[0] = userGoal;
     userData.goal[1] = goalVal.value;
-    console.log(userData);
+
+    let userBMRInfo = getBMR();
+
+    userData.BMR = userBMRInfo;
+
+    // clears input fields after data has been stored in user object on submit
+    nameInput.value = '';
+    genderInput.value = '';
+    weightInput.value = '';
+    heightInput.value = '';
+    ageInput.value = '';
+    goalVal.classList.add('hidden');
+    for (let i = 0; i < goalButtons.length; i++) {
+        goalButtons[i].checked = false;
+    }
 }
 
 function getUserGoal() {
@@ -44,6 +60,30 @@ function getUserGoal() {
             return goalButtons[i].value;
         }
     }
+}
+
+// Uses the Revised Harris-Benedict Equation (is an estimate)
+function getBMR() {
+    let maleBMR = Math.round(88.362 + (13.397 * toKilograms(userData.weight)) + (4.799 * toCentimeters(userData.height)) - (5.677 * userData.age));
+    let femaleBMR = Math.round(447.593 + (9.247 * toKilograms(userData.weight)) + (3.098 * toCentimeters(userData.height)) - (4.330 * userData.age));
+
+    if (userData.gender === 'male') {
+        return maleBMR;
+    } else {
+        return femaleBMR;
+    }
+}
+
+// converts pounds to kilograms
+function toKilograms(pounds) {
+    let kilograms = pounds * kilogramPerPound;
+    return kilograms;
+}
+
+// converts feet to centimeters
+function toCentimeters(feet) {
+    let centimeters = feet * centimeterPerFoot;
+    return centimeters;
 }
 
 goalButtons.forEach(item => {
