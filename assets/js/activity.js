2867df553ffac553d31f1de60bba28a6aabd0a6e
LorenzoF <lorenzoferritsic@gmail.com>
2022-07-09 13:18:31 -0400

    Made user form functionality on homepage

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -3,5 +3,6 @@ var nextButton = document.getElementById("activityNextPageBtn")
 function gohome() {
     document.location = "index.html"
 }
+
 nextButton.addEventListener("click", gohome)
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ let userData = {
     age: null, // will be populated on submit of userform
     goal: ['', null], // will be populated on submit of userform
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
