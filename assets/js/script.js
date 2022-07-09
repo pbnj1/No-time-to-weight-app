98353931e4c64fdc79173548372c755c0c6e69ba
Dalian Grullon <dalian.business@aol.com>
2022-07-09 12:09:49 -0400

    created userData object

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let userData = {
+    gender: '', // will be populated on submit of userform
+    weight: null, // will be populated on submit of userform
+    height: null, // will be populated on submit of userform
+    age: null, // will be populated on submit of userform
+    goal: ['', null], // will be populated on submit of userform
+}
