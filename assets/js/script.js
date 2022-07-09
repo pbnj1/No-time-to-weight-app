f7975846d74f0644eabe3c6ee258128385338559
Dalian Grullon <dalian.business@aol.com>
2022-07-09 15:54:07 -0400

    added a goal value function that responds to each radio button's click and adds an input field to record user goal data

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-let userData = {
-    gender: '', // will be populated on submit of userform
-    weight: null, // will be populated on submit of userform
-    height: null, // will be populated on submit of userform
-    age: null, // will be populated on submit of userform
-    goal: ['', null], // will be populated on submit of userform
-}
--- assets/js/userForm.js
+++ assets/js/userForm.js
@@ -5,6 +5,24 @@ let heightInput = document.getElementById('height');
 let weightInput = document.getElementById('weight');
 let genderInput = document.getElementById('gender');
 let goalButtons = document.getElementsByName('fit_goal'); // is an array
+let goalValEl = document.getElementById('goal-val');
+let goalVal = document.createElement('input');
+
+
+function addGoalValue() {
+    for (let i = 0; i < goalButtons.length; i++) {
+        if (goalButtons[i].checked && (goalButtons[i].value !== 'maintain')) {
+            goalVal.setAttribute('placeholder', 'How many pounds?');
+            goalVal.setAttribute('type', 'number');
+            goalValEl.append(goalVal);
+            goalVal.classList.remove('hidden');
+            goalVal.value = '';
+        } else if (goalButtons[i].checked && (goalButtons[i].value === 'maintain')) {
+            goalVal.classList.add('hidden');
+            goalVal.value = 0;
+        }
+    }
+}
 
 function updateUserObject(e){
     e.preventDefault();
@@ -15,6 +33,8 @@ function updateUserObject(e){
     userData.height = heightInput.value;
     userData.age = ageInput.value;
     userData.goal[0] = userGoal;
+    userData.goal[1] = goalVal.value;
+    console.log(userData);
 }
 
 function getUserGoal() {
@@ -25,4 +45,7 @@ function getUserGoal() {
     }
 }
 
+goalButtons.forEach(item => {
+    item.addEventListener('click', addGoalValue);
+})
 userFormEl.addEventListener('submit', updateUserObject);
