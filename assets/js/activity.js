ee5337706be7dd0f78768244e82da591312fde6e
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-07-09 21:01:12 -0400

    working on the fetch for activities

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -1,69 +1,51 @@
-var nextButton = document.getElementById("activityNextPageBtn")
-var exVal = document.querySelector('input[id = "ex-choice"]')
+var nextButton = document.getElementById("activityNextPageBtn");
+var exVal = document.querySelector('input[id = "ex-choice"]');
 
 function gohome() {
-    document.location = "index.html"
+  document.location = "index.html";
 }
 
-$( function() {
-    var handle = $( "#custom-handle" );    
-    var availableTags = [
-        "Run",
-        "Walk",
-        "Darts",
-        "Shuffleboard",
-        "Raking lawn",
-        "Aerobics",
-        "Stairs",
-        "Steps",
-        "Rowing",
-        "Cycling",
-        "Bicycling",
-      ];
+$(function () {
+  var handle = $("#custom-handle");
+  var availableTags = [
+    "Run",
+    "Walk",
+    "Darts",
+    "Shuffleboard",
+    "Raking lawn",
+    "Aerobics",
+    "Stairs",
+    "Steps",
+    "Rowing",
+    "Cycling",
+    "Bicycling",
+  ];
 
-    $( "#exerciseSearch" ).autocomplete({
-        source: availableTags
-      });
+  $("#exerciseSearch").autocomplete({
+    source: availableTags,
+  });
 
-    //Assigns current slider value to the slider for user visual
-    $( "#slider" ).slider({
-        value:30,
-        max: 120,
-        create: function() {
-            handle.text( $( this ).slider( "value" ) );
-        },
-        slide: function( event, ui ) {
-            handle.text( ui.value ); 
-        }
-    });
-  } );
+  //Assigns current slider value to the slider for user visual
+  $("#slider").slider({
+    value: 30,
+    max: 120,
+    create: function () {
+      handle.text($(this).slider("value"));
+    },
+    slide: function (event, ui) {
+      handle.text(ui.value);
+    },
+  });
+});
 
+nextButton.addEventListener("click", gohome);
 
-nextButton.addEventListener("click", gohome)
-
-
-
-var mealSxn = document.getElementById("sgst-returned");
-var mealVal = document.querySelector('input[id = "meal-amt"]');
-var snackVal = document.querySelector('input[id = "snack-amt"]');
-var mealBtn = document.getElementById("mealBtn");
-
-function mealDisplay() {
-  if (mealVal.value === "" || mealVal.value <= 0) {
-    document.getElementById("meal-amt").focus();
-  } else {
-    mealSxn.style.display = "flex";
-  }
-}
-
-
-function exDisplay(){
-  if (exVal.value ===""){
+function exDisplay() {
+  if (exVal.value === "") {
     document.getElementById("ex-choice").focus();
-  }else(document.getElementById("ex-sgst").style.display = "flex")
-
+  } else document.getElementById("ex-sgst").style.display = "flex";
 }
 
+exBTN.addEventListener("click", exDisplay);
 
-
-exBTN.addEventListener("click", exDisplay)
+FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu
