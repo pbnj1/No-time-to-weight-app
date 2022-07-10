eb8bb48fb68718fa3cdfd88d988d005a29cd766c
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-07-10 13:19:57 -0400

    added in conditional for no search results

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -52,41 +52,54 @@ function exDisplay() {
 
 exBTN.addEventListener("click", exDisplay);
 
-
-
-
-function exSearch(){
-  $("#ex-sgst").empty()
-  var val =$('#slider').slider("value")
-  var queryURL = "https://api.api-ninjas.com/v1/caloriesburned?&activity=" + exVal.value + "&duration=" + val;
+function exSearch() {
+  $("#ex-sgst").empty();
+  var val = $("#slider").slider("value");
+  var queryURL =
+    "https://api.api-ninjas.com/v1/caloriesburned?&activity=" +
+    exVal.value +
+    "&duration=" +
+    val;
   fetch(queryURL, {
-  
-    headers:{ "X-Api-Key": "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu"}
+    headers: { "X-Api-Key": "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu" },
   })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-      
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+       
 
-        for (let i =0; i<3; i++){
-          console.log(data[i].name)
-
-          // if(card3 !== ""){
-           
-          // }
-
-          var exercise1 = document.createElement("p")
-          exercise1.setAttribute("class", "flex items-center justify-center w-full h-1/4 border-2 border-black ml-2 mt-6 mr-2 mb-2")
-          exercise1.textContent =  "Your first suggested exercise is to " + data[i].name + ".  Great Pick! You will burn " + data[i].total_calories + " calories doing this for " + val + " minutes!";
-          card3.append(exercise1)
-
-          
+      for (let i = 0; i < 3; i++) {
+       
+        if ((data.length === 0)) {
+          var displayMessage = document.createElement("p");
+          displayMessage.setAttribute(
+            "class",
+            "flex items-center justify-center w-full h-1/4 border-2 border-black ml-2 mt-6 mr-2 mb-2"
+          );
+          displayMessage.textContent =
+            "We couldn't find that exercise!  Please try again.";
+  
+          card3.append(displayMessage);
         }
+  
 
-      });
-     
+        var exercise1 = document.createElement("p");
+        exercise1.setAttribute(
+          "class",
+          "flex items-center justify-center w-full h-1/4 border-2 border-black ml-2 mt-6 mr-2 mb-2"
+        );
+        exercise1.textContent =
+          "Your first suggested exercise is to " +
+          data[i].name +
+          ".  Great Pick! You will burn " +
+          data[i].total_calories +
+          " calories doing this for " +
+          val +
+          " minutes!";
+        card3.append(exercise1);
+      }
+    });
 }
 
 exBTN.addEventListener("click", exSearch);
