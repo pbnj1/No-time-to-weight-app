f88b21b6c2ffc84ec03fd292c7e50d2e6c41a7eb
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-07-09 22:21:56 -0400

    getting dynamic feedback from the API on exercise

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -1,6 +1,7 @@
 var nextButton = document.getElementById("activityNextPageBtn");
 var exVal = document.querySelector('input[id = "ex-choice"]');
 var Key = "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu";
+var card1 = document.getElementById("exercise-holder");
 
 function gohome() {
   document.location = "index.html";
@@ -50,11 +51,10 @@ function exDisplay() {
 exBTN.addEventListener("click", exDisplay);
 
 
-exBTN.addEventListener("click", exSearch);
+
 
 function exSearch(){
   var val =$('#slider').slider("value")
-  console.log(val)
   var queryURL = "https://api.api-ninjas.com/v1/caloriesburned?&activity=" + exVal.value + "&duration=" + val;
   fetch(queryURL, {
     headers:{ "X-Api-Key": "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu"}
@@ -64,6 +64,18 @@ function exSearch(){
       })
       .then(function (data) {
         console.log(data)
-        });
+        console.log(data[0].name)
+        console.log(data[0].total_calories)
+        
+
+        for (let i =0; i<3; i++){
+          console.log(data)
+          var exercise1 = document.createElement("div")
+          exercise1.textContent =  "your first exercise is to " + exVal.value + ".  Great Pick! You will burn " + data[0].total_calories + "!";
+          card1.append(exercise1)
+        }
+      });
 
 }
+
+exBTN.addEventListener("click", exSearch);
