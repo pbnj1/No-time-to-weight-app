2887b1d860a6db0a4597a7f7c52c713464079f57
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-07-10 12:39:31 -0400

    set up for loop to create elements and append API info

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -1,7 +1,9 @@
 var nextButton = document.getElementById("activityNextPageBtn");
 var exVal = document.querySelector('input[id = "ex-choice"]');
 var Key = "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu";
-var card1 = document.getElementById("exercise-holder");
+var card1 = document.getElementById("exercise-holder1");
+var card2 = document.getElementById("exercise-holder2");
+var card3 = document.getElementById("ex-sgst");
 
 function gohome() {
   document.location = "index.html";
@@ -57,6 +59,7 @@ function exSearch(){
   var val =$('#slider').slider("value")
   var queryURL = "https://api.api-ninjas.com/v1/caloriesburned?&activity=" + exVal.value + "&duration=" + val;
   fetch(queryURL, {
+  
     headers:{ "X-Api-Key": "FbfFoXfcu1pqZGL1wfm5ng==s1ZwGIbhzW13ihcu"}
   })
       .then(function (response) {
@@ -64,18 +67,40 @@ function exSearch(){
       })
       .then(function (data) {
         console.log(data)
-        console.log(data[0].name)
-        console.log(data[0].total_calories)
+        // console.log(data[0].name)
+        // console.log(data[0].total_calories)
         
 
         for (let i =0; i<3; i++){
-          console.log(data)
-          var exercise1 = document.createElement("div")
-          exercise1.textContent =  "your first exercise is to " + exVal.value + ".  Great Pick! You will burn " + data[0].total_calories + "!";
-          card1.append(exercise1)
-        }
-      });
+          console.log(data[i].name)
 
+          
+          var exercise1 = document.createElement("p")
+          exercise1.setAttribute("class", "flex items-center justify-center w-full h-1/4 border-2 border-black ml-2 mt-6 mr-2 mb-2")
+          exercise1.textContent =  "Your first suggested exercise is to " + data[i].name + ".  Great Pick! You will burn " + data[i].total_calories + " calories doing this for " + val + " minutes!";
+          card3.append(exercise1)
+
+        }
+
+      });
+     
 }
 
 exBTN.addEventListener("click", exSearch);
+
+
+
+ // var exercise1 = document.createElement("p")
+          // exercise1.setAttribute("class", "flex items-center justify-center w-full h-1/4 border-2 border-black ml-2 mt-6 mr-2 mb-2")
+          // exercise1.textContent =  "your first suggested exercise is to " + data[0].name + ".  Great Pick! You will burn " + data[0].total_calories + " calories !";
+          // card3.append(exercise1)
+
+          // var exercise2 = document.createElement("p")
+          // exercise2.setAttribute("class", "flex items-center justify-center w-full h-1/4 border-2 border-black ml-2 mt-6 mr-2 mb-2")
+          // exercise2.textContent =  "your second suggested exercise is to " + data[1].name + ".  Great Pick! You will burn " + data[1].total_calories + " calories !";
+          // card3.append(exercise2)
+
+          // var exercise3 = document.createElement("p")
+          // exercise3.setAttribute("class", "flex items-center justify-centerw-full h-1/4 border-2 border-black ml-2 mt-6 mr-2 mb-2")
+          // exercise3.textContent =  "your second suggested exercise is to " + data[2].name + ".  Great Pick! You will burn " + data[2].total_calories + " calories !";
+          // card3.append(exercise3)
