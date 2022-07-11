b3425540108f147822125fc8b68dc76da0f8f897
Dalian Grullon <dalian.business@aol.com>
2022-07-11 12:29:55 -0400

    created logic to display summary page if user is found in local storage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,2 +1,18 @@
 document.getElementById("startBTN").onclick = function(){location.href = "userForm.html"}
 
+function findUser(e) {
+    e.preventDefault();
+
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) === document.getElementById('retrieve-name').value) {
+            console.log('match');
+            document.getElementById('retrieval-page').classList.add('z-0', 'hidden');
+            document.getElementById('summary-page').classList.remove('hidden');
+        } else {
+            document.getElementById('retrieval-page').classList.add('z-0', 'hidden');
+            document.getElementById('get-started').classList.remove('hidden');
+        }
+    }
+}
+
+document.getElementById('user-retrieve').addEventListener('submit', findUser);
