0c8f1f1fcc0838926adfe02fa817467f1d9bca87
Dalian Grullon <dalian.business@aol.com>
2022-07-11 13:10:43 -0400

    created a function to populate the summary page if user is found in storage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,10 +2,9 @@ document.getElementById("startBTN").onclick = function(){location.href = "userFo
 
 function findUser(e) {
     e.preventDefault();
-
+    let currentUser = document.getElementById('retrieve-name').value;
     for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) === document.getElementById('retrieve-name').value) {
-            console.log('match');
+        if (localStorage.key(i) === currentUser) {
             document.getElementById('retrieval-page').classList.add('z-0', 'hidden');
             document.getElementById('summary-page').classList.remove('hidden');
         } else {
@@ -13,6 +12,27 @@ function findUser(e) {
             document.getElementById('get-started').classList.remove('hidden');
         }
     }
+
+    populateSummary(currentUser);
+}
+
+function populateSummary(user) {
+    let userInfo = JSON.parse(localStorage.getItem(user));
+    let retrievedUser = document.getElementById('retrieved-user');
+    let userGoal = document.getElementById('goal');
+    let userDietCal = document.getElementById('dietCal');
+    let userExerciseCal = document.getElementById('exerciseCal');
+    let userDeficit = document.getElementById('deficit');
+    let userGoalTime = document.getElementById('goalTime');
+
+
+    retrievedUser.textContent = userInfo.name;
+    userGoal.textContent = userInfo.goal[0];
+    userDietCal.textContent = userInfo.dietCal;
+    userExerciseCal.textContent = userInfo.exerciseCal;
+    userDeficit.textContent = userInfo.deficit;
+    userGoalTime.textContent = userInfo.goalTime;
+
 }
 
 document.getElementById('user-retrieve').addEventListener('submit', findUser);
